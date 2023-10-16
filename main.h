8bf6876686b4e082e7f30bc5a17f5f067dd74a87
gitere001 <sasmuypwa2019@gmail.com>
2023-10-16 01:44:24 -0700

    function that produces output according to a format

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlenc(const char *str);
+int _strlen(char *str);
+int print_37(void);
+#endif
---
+++ print_str.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string
+ * @str: string pointer
+ * Return: length of a string
+ */
+int _strlen(char *str)
+{
+	int i;
+	for (i = 0; str[i] != 0; i++)
+	{
+
+	}
+		return (i);
+	
+}
+/**
+ * _strlenc - strlen function but applied for constant char pointer
+ * @str: char pointer
+ */
+int _strlenc(const char *str)
+{
+	int i;
+	for (i = 0; str[i] != 0; i++)
+	{
+
+	}
+		return (i);
+	
+}
